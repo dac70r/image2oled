3f1c3f7403f01f28e33a6004fd30abcfdeb72f92
Dennis Wong <denniswong178@gmail.com>
2022-03-31 16:16:04 +0800

    Added too_loud.png

--- image2oled.py
+++ image2oled.py
@@ -13,7 +13,7 @@ b = 0;
 c = 0;
 
 
-im = imageio.imread('lea_seydoux.png') #read image, convert to data and store in array
+im = imageio.imread('too_loud_resized.png') #read image, convert to data and store in array
 print(im.shape) #shows the dimensions of the read image
 
 # print(64*96)
